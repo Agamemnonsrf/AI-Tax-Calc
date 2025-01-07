f9904ec5b4d05e8aafaf676f5c8002b83b905469
Agamemnon <aserafeim@deloitte.gr>
2025-01-07 18:24:55 +0200

    added backend tests with jest and supertest

--- ai-tax-calc-backend/dist/server.js
+++ ai-tax-calc-backend/dist/server.js
@@ -5,53 +5,54 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const util_1 = require("./util");
-const openai_1 = __importDefault(require("openai"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// Load environment variables from .env file
+const calculate_tax_1 = __importDefault(require("./routes/calculate-tax/calculate-tax"));
+const auth_1 = __importDefault(require("./routes/auth/auth"));
+const tax_advisor_1 = __importDefault(require("./routes/tax-advisor/tax-advisor"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const db_1 = require("./routes/auth/db");
 dotenv_1.default.config();
-// Create an Express app
 const app = (0, express_1.default)();
-// Middleware
 app.use((0, cors_1.default)());
-app.use(express_1.default.json()); // Parse JSON bodies
-// Example route
+app.use(body_parser_1.default.json());
+const envVars = [
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    process.env.DB_NAME,
+    process.env.DB_HOST,
+    process.env.DB_PORT,
+];
+//not required, only used for chat and auth
+//process.env.DB_SECRET_KEY,
+//process.env.OPENAI_API_KEY
+if (envVars.some((v) => !v)) {
+    console.error("Missing required environment variables. Check your .env file.: ", envVars);
+    process.exit(1);
+}
 app.get("/", (req, res) => {
     res.send("Welcome to the TypeScript Express server!");
 });
-app.post("/calculate-tax", (req, res) => {
-    const formData = req.body;
-    const result = (0, util_1.calculateTax)(formData);
-    res.json(result);
-});
-app.post("/tax-advice", async (req, res) => {
-    const payload = req.body;
-    console.log(payload.question);
-    console.log(process.env.OPENAI_API_KEY);
-    const openai = new openai_1.default({ apiKey: process.env.OPENAI_API_KEY });
-    const competion = await openai.chat.completions.create({
-        model: "gpt-4o-mini",
-        messages: [
-            {
-                role: "system",
-                content: "You are a helpful tax advisor. You answer plainly, straightforwardly, to the point, with short and easy to read responses for the user. Use markdown to format your responses.",
-            },
-            ...payload.history,
-            {
-                role: "user",
-                content: payload.question,
-            },
-        ],
-    });
-    res.json({ answer: competion.choices[0].message.content });
-});
-// Error handling middleware (optional)
+(async () => {
+    await (0, db_1.createUsersTable)();
+    await (0, db_1.createChatSessionsTable)();
+    await (0, db_1.createChatMessagesTable)();
+})();
+app.use("/auth", auth_1.default);
+app.use("/calculate-tax", calculate_tax_1.default);
+app.use("/tax-advisor", tax_advisor_1.default);
 app.use((err, req, res, next) => {
     console.error(err.message);
     res.status(500).json({ error: "Internal Server Error" });
 });
-// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    }
+    catch (error) {
+        console.error('Error starting server: ', error);
+    }
+};
+startServer();
